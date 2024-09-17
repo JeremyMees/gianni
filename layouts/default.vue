5bacb8d0125e6ffc53539f123aa2abb842f83139
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-09-17 20:14:46 +0200

    feat: added project structure

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <!-- <Navigation /> -->
+
+    <main class="relative grow pb-[140px] flex flex-col gap-[80px] overflow-hidden">
+      <slot />
+    </main>
+
+    <!-- <Footer /> -->
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout>
+    <div class="font-bold">
+      hello world!
+    </div>
+  </NuxtLayout>
+</template>
