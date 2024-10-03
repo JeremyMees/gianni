f009ef22e2427afcc9588bb41baf7dec1c1999c8
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-10-03 16:59:05 +0200

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,58 @@
-export default defineAppConfig({})
+export default defineAppConfig({
+  footer: {
+    links: [{
+      title: "Contact",
+
+      links: [{
+        label: "info.kaminomedia@gmail.com",
+        link: "mailto:info.kaminomedia@gmail.com",
+        icon: "lucide:mail"
+      }, {
+        label: "+32 471 07 21 47",
+        link: "tel:+32471072147",
+        icon: "lucide:phone"
+      }, {
+        label: "BTW BE 1014.413.132.",
+        icon: "lucide:factory"
+      }]
+    }, {
+      title: "Socials",
+
+      links: [{
+        label: "Instagram",
+        link: "https://www.instagram.com/kamino.media?igsh=cHQ5b2tseHZldXFy",
+        icon: "lucide:instagram"
+      }, {
+        label: "Linkedin",
+        link: "https://www.linkedin.com/company/kamino-media/",
+        icon: "lucide:linkedin"
+      }]
+    }, {
+      title: "Pagina's",
+
+      links: [{
+        label: "Bruiloften",
+        link: "/bruiloften"
+      }, {
+        label: "Over ons",
+        link: "/#about"
+      }, {
+        label: "Prijzen",
+        link: "/prijzen"
+      }]
+    }]
+  },
+
+  navigation: {
+    links: [{
+      label: "Bruiloften",
+      link: "/bruiloften"
+    }, {
+      label: "Over ons",
+      link: "/#about"
+    }, {
+      label: "Prijzen",
+      link: "/prijzen"
+    }]
+  }
+})
