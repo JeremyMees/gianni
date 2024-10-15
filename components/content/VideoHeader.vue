622ae4511f0edbda0070b9572434ea78a723e08e
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-10-15 20:35:32 +0200

    fix: video header z index

--- components/content/VideoHeader.vue
+++ components/content/VideoHeader.vue
@@ -19,13 +19,13 @@ defineProps<{
       Your browser does not support the video tag.
     </video>
     <div class="absolute z-[2] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-8 text-white w-full px-5 md:px-20">
+      <h1 class="invisible">
+        {{ title }}
+      </h1>
       <SvgoTextLogo class="min-w-full min-h-[100px] md:min-w-[600px] md:min-h-[100px] text-white" />
       <span class="text-xl font-bold text-center drop-shadow-xl">
         {{ subtitle }}
       </span>
     </div>
-    <h1 class="absolute">
-      {{ title }}
-    </h1>
   </section>
 </template>
