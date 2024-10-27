fa0ac1d4c146e7c0f62f41510cd89d136f8cf1fe
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-10-27 16:00:12 +0100

    fix: testimonials random picker

--- utils/index.ts
+++ utils/index.ts
@@ -8,9 +8,24 @@ export function splitArray<T>(arr: T[], size: number): T[][] {
   return result
 }
 
+export function shuffleArray<T>(arr: T[]): T[] {
+  for (let i = 0; i < arr.length; i++) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
+
+  return arr
+}
+
 export function randomArrayItems<T>(arr: T[], number = 1): T[] {
   const result: T[] = []
   const copy = [...arr]
+
+  if (number >= arr.length) {
+    return shuffleArray(arr)
+  }
 
   for (let i = 0; i < number; i++) {
     const index = Math.floor(Math.random() * copy.length)
